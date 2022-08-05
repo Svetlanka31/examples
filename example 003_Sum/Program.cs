51d12a193366fa63ae193513cccf87d2dc914350
Svetlana <kozhevnikova-svetlana85@bk.ru>
2022-08-05 20:08:27 +0300

    Add example 006

--- example 003_Sum/Program.cs
+++ example 003_Sum/Program.cs
@@ -1,4 +1,4 @@
-﻿int numberA = new Random .Next(1, 10);// 1,2,3..9
-int numberB = new Random .Next(1, 10);
+﻿int numberA = new Random.Next(1, 10);// 1,2,3..9
+int numberB = new Random.Next(1, 10);
 int result = numberA + numberB;
 Console.WriteLine(result);
